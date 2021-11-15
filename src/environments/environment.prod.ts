323739705f2a640586f008c700d001deecc06d38
Diego Fernando Gómez <d.fernandogomez94@gmail.com>
2021-11-15 11:33:55 -0500

    actualizacion 2 11/15

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   appId: '379e284eca011a8f00687e715b92e777',
-  baseUrl: 'https://'
+  baseUrl: 'http://',
+  username: 'diegoser09',
+  geonamesApi: 'api',
 };
